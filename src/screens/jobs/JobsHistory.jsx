64c94b31718c18ad3f42cf7b160176b6bf6bc497
divyanshyadav87 <dsy16325@gmail.com>
2026-07-13 01:54:17 +0530

    Refactor JobsHistory component; wrap MyJobList in SafeAreaView for improved layout and safety on devices

--- src/screens/jobs/JobsHistory.jsx
+++ src/screens/jobs/JobsHistory.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import TopNav from '../../components/TopNav';
 import MyJobList from '../../components/MyJobsList';
 
@@ -7,7 +8,9 @@ export default function JobsHistory() {
   return (
     <View style={styles.container}>
       <TopNav />
-      <MyJobList />
+      <SafeAreaView style={styles.flex} edges={['bottom']}>
+        <MyJobList />
+      </SafeAreaView>
     </View>
   );
 }
@@ -16,5 +19,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F9F8F7',
+  },
+  flex: {
+    flex: 1,
   },
 });
